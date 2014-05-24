88a50f980b787b7434a34f7119eac0e6a5f1d2ce
Justin <Justin@Justins-MacBook-Pro.local>
2014-05-24 12:19:41 -0500

    Built a todo list and added a field to add new todo items to the list.

--- index.php
+++ index.php
@@ -1,1 +1,31 @@
-hello world!!
+<!DOCTYPE html>
+<html>
+	<head>
+			<meta charset="utf-8">
+			<title>TODO List</title>
+	</head>	
+	<body>
+	<?php
+    var_dump($_GET);
+    var_dump($_POST);
+    ?>
+		<h3>TODO List</h3>
+			<ul>
+				<li>Learn LINUX</li>
+				<li>Learn APACHE</li>
+				<li>Learn MYSQL</li>
+				<li>Learn PHP</li>
+			</ul>	
+	</body>	
+	<form method="POST" Action=/"index.php">
+		<p>
+			<label for="todo_item">New todo item</label>
+			<textarea id="todo_item" name="todo_item" rows="1" cols="40" type="text">
+			</textarea>
+		</p>
+		<p>	
+			<input type="submit" value="add">		
+		</p>
+	</form>
+
+</html>
