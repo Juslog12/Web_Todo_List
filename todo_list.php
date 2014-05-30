ae715d616c1c8b7a13251a6f26ff5b2bc580290d
Justin <Justin@Justins-MacBook-Pro.local>
2014-05-30 15:07:15 -0500

    Adde the ability to upload files and merge the list from uploaded file with the current list

--- todo_list.php
+++ todo_list.php
@@ -1,5 +1,5 @@
 <?php
-
+var_dump($_FILES);
 define('FILENAME', 'data/list.txt');
 
 $list_items =[];
@@ -16,18 +16,41 @@ function readtheFile($filename) {
 	
 	$array = [];
 
-	if (filesize($filename) > 0) {
-	 	if (is_readable($filename)) {
+	if (is_readable($filename) && (filesize($filename)) > 0) {
 			$handle = fopen($filename, 'r');
 			$contents = trim(fread($handle, filesize($filename)));
 			$array = explode(PHP_EOL, $contents);
 			fclose($handle);
-		}
-	}
 
+		}
 	return $array;
 }
 
+$list_items = readtheFile(FILENAME);
+
+if($_POST) {
+	array_push($list_items, $_POST['todo_item']);
+}
+
+if (isset($_GET['removeIndex'])) {
+	$index = $_GET['removeIndex'];
+	unset($list_items[$index]);
+}
+
+if(count($_FILES) > 0 && $_FILES['file1']['error'] == 0) {
+	$upload_dir = '/vagrant/sites/todo.dev/public/uploads/';
+	$filename = basename($_FILES['file1']['name']);
+	$saved_filename = $upload_dir . $filename; 
+	move_uploaded_file($_FILES['file1']['tmp_name'], $saved_filename);
+	$new_items = readtheFile($saved_filename);
+
+	$list_items = array_merge($list_items, $new_items);
+
+} 
+
+if(isset($saved_filename)) {
+	echo "<p>You can download your file <a href='/uploads/{$filename}'>here</a>.</p>";
+}
 
 ?>
 <!DOCTYPE html>
@@ -43,16 +66,6 @@ function readtheFile($filename) {
     ?>
 		<h3>TODO List</h3>
 <?php
-		$list_items = readtheFile(FILENAME);
-
-		if($_POST) {
-			array_push($list_items, $_POST['todo_item']);
-		}
-
-		if (isset($_GET['removeIndex'])) {
-			$index = $_GET['removeIndex'];
-			unset($list_items[$index]);
-		}
 
 		foreach ($list_items as $index => $item) {
 			echo "<li>$item <a href=\"todo_list.php?removeIndex=$index\">Remove Item</a></li>";
@@ -72,7 +85,20 @@ function readtheFile($filename) {
 		<p>	
 			<input type="submit" value="Add">		
 		</p>
+
 	</form>
+	
+	<h1>Upload File</h1>
+
+<form method="POST" enctype="multipart/form-data">
+    <p>
+        <label for="file1">File to upload: </label>
+        <input type="file" id="file1" name="file1">
+    </p>
+    <p>
+        <input type="submit" value="Upload">
+    </p>
+</form>
 
 	</body>	
 </html>
